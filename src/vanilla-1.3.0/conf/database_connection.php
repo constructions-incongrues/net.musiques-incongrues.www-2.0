c6f00865fefaac392c617b186073783375c1904f
Tristan Rivoallan <tristan@rivoallan.net>
2013-03-01 13:54:07 +0100

    [vanilla] extracted database connection configuration into dist file

---
+++ src/vanilla-1.3.0/conf/database_connection.php
@@ -0,0 +1,5 @@
+// Database Configuration Settings
+$Configuration['DATABASE_HOST'] = 'localhost';
+$Configuration['DATABASE_NAME'] = 'net_musiquesincongrues_www';
+$Configuration['DATABASE_USER'] = 'root';
+$Configuration['DATABASE_PASSWORD'] = '';
