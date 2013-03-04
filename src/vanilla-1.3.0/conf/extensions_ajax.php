d092cf9f94dfa3eb0e78b938de7ca54bed5363a6
Tristan Rivoallan <tristan@rivoallan.net>
2013-03-04 10:32:05 +0100

    [CiElasticsearch] extension must be active for ajax requests too

--- src/vanilla-1.3.0/conf/extensions_ajax.php
+++ src/vanilla-1.3.0/conf/extensions_ajax.php
@@ -2,3 +2,6 @@
 // Make sure this file was not accessed directly and prevent register_globals configuration array attack
 if (!defined('IN_VANILLA')) exit();
 // Enabled Extensions
+
+// Constructions Incongrues custom extensions
+include($Configuration['EXTENSIONS_PATH']."CiElasticsearch/default.php");
