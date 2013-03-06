b9868dc5357647f0d4940c297ed0f8e2ab713070
Tristan Rivoallan <tristan@rivoallan.net>
2013-03-06 17:49:28 +0100

    [CiTwig] No need to duplicate parent code

--- src/vanilla-1.3.0/extensions/CiTwig/src/ConstructionsIncongrues/Vanilla/CategoryManager.php
+++ src/vanilla-1.3.0/extensions/CiTwig/src/ConstructionsIncongrues/Vanilla/CategoryManager.php
@@ -5,16 +5,8 @@ class CategoryManager extends \CategoryManager
 {
 	public function GetCategories($IncludeCount = '0', $OrderByPreference = '0', $ForceRoleBlock = '1')
 	{
-		$OrderByPreference = ForceBool($OrderByPreference, 0);
-		$s = $this->GetCategoryBuilder($IncludeCount, $ForceRoleBlock);
-		if ($OrderByPreference && $this->Context->Session->UserID > 0) {
-			// Order by the user's preference (unblocked categories first)
-			$s->AddOrderBy('Blocked', 'b', 'asc');
-		}
-		$s->AddOrderBy('Priority', 'c', 'asc');
-
 		// Create categories array from database results
-		$dbResults = $this->Context->Database->Select($s, $this->Name, 'GetCategories', 'An error occurred while retrieving categories.');
+		$dbResults = parent::GetCategories($IncludeCount, $OrderByPreference, $ForceRoleBlock);
 		$categories = array();
 		while ($Row = $this->Context->Database->GetRow($dbResults)) {
 			$Category = $this->Context->ObjectFactory->NewObject($this->Context, 'Category');
