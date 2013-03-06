09c08119226e5fbc8e01ce453bbd51a7b735e674
Tristan Rivoallan <tristan@rivoallan.net>
2013-03-06 09:59:45 +0100

    [CiTwig] new CiTwigRender() helper function

--- src/vanilla-1.3.0/extensions/CiTwig/default.php
+++ src/vanilla-1.3.0/extensions/CiTwig/default.php
@@ -2,14 +2,25 @@
 // Composer autoload
 require($Context->Configuration['APPLICATION_PATH'].'/../../vendor/autoload.php');
 
+/**
+ * Helper function to be used in templates.
+ * eg. in menu.php : CiTwigRender(__FILE__, $this);
+ */
+function CiTwigRender($path, $control)
+{
+	echo $control->Context->Twig->render(basename($path), array('self' => $control)); 
+}
+
 // Instanciate and configure Twig loader
 // TODO : handle debug mode
 // TODO : inject common context values (request, etc)
+// TODO : rename "views" folder to "templates/twig" or similar
+// TODO : allow extensions to declare more twig paths for cascading
 $twig = new \Twig_Environment(new \Twig_Loader_Filesystem(__DIR__.'/themes/vanilla/views'));
 
 // Add functions
 // TODO : make list configurable
-$twigImportFunctions = array('AppendUrlParameters', 'GetRequestUri');
+$twigImportFunctions = array('AppendUrlParameters', 'GetRequestUri', 'GetDynamicCheckBox','ForceIncomingString', 'GetUrl');
 foreach ($twigImportFunctions as $functionName) {
 	$twig->addFunction(new Twig_SimpleFunction($functionName, function () use ($functionName) {
 		return call_user_func_array($functionName, func_get_args());
