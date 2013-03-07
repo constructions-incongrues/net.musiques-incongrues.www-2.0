963d6002437a8090349a9927eb8511a2d28d4249
Tristan Rivoallan <tristan@rivoallan.net>
2013-03-07 16:29:57 +0100

    [CiTwig] ported vanilla/comments_foot.php

---
+++ src/vanilla-1.3.0/extensions/CiTwig/themes/vanilla/views/comments_foot.php
@@ -0,0 +1,4 @@
+<div class="ContentInfo Bottom">
+	<a href="{{ GetUrl(self.Context.Configuration, 'index.php') }}" class="left" id="BackToDiscussions">{{ self.Context.GetDefinition('BackToDiscussions') }}</a>
+	<a href="{{ GetRequestUri() }}#pgtop" id="TopOfPage">{{ self.Context.GetDefinition('TopOfPage') }}</a>
+</div>
